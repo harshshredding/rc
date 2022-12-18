d086060e4819ec1ec7c59bf709ecd711d91dc72a
Harsh Verma <harshshredding@gmail.com>
2022-12-17 21:54:20 -0500

    vi commands for terminal

--- harsh_rc.sh
+++ harsh_rc.sh
@@ -1,3 +1,5 @@
+set -o vi
+
 export HISTCONTROL=ignoredups:erasedups  # no duplicate entries
 export HISTSIZE=100000                   # big big history
 export HISTFILESIZE=100000               # big big history
